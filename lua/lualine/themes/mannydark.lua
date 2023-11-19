bcae437b852ac07edb0875b99c3ae0bbcd9c798d
Manny <mannythefay@gmail.com>
2023-11-19 17:10:42 +0100

    Update inactive buffer for Lualine

--- lua/lualine/themes/mannydark.lua
+++ lua/lualine/themes/mannydark.lua
@@ -1,50 +1,46 @@
--- [[ MannyDark theme for LuaLine ]]
--- [[ License: MIT                ]]
--- [[ mannythefay@gmail.com       ]]
--- [[ https://github.com/MannyFay ]]
-
 local colors = {
   blue       = '#569CD6',
   turquoise  = '#45C8B0',
   purple     = '#C064C7',
   orange     = '#E8BF6A',
-  gray       = '#252525',
+  gray       = '#606366',
+  grayDark  = '#252525',
 }
 
 return {
   normal = {
-    a = { fg = colors.blue, bg = colors.gray },
-    b = { fg = colors.blue, bg = colors.gray },
-    c = { fg = colors.blue, bg = colors.gray },
-    x = { fg = colors.blue, bg = colors.gray },
-    y = { fg = colors.blue, bg = colors.gray },
-    z = { fg = colors.blue, bg = colors.gray },
+    a = { fg = colors.blue, bg = colors.grayDark },
+    b = { fg = colors.blue, bg = colors.grayDark },
+    c = { fg = colors.blue, bg = colors.grayDark },
+    x = { fg = colors.blue, bg = colors.grayDark },
+    y = { fg = colors.blue, bg = colors.grayDark },
+    z = { fg = colors.blue, bg = colors.grayDark },
   },
   insert = {
-    a = { fg = colors.purple, bg = colors.gray },
-    b = { fg = colors.purple, bg = colors.gray },
-    c = { fg = colors.purple, bg = colors.gray },
-    x = { fg = colors.purple, bg = colors.gray },
-    y = { fg = colors.purple, bg = colors.gray },
-    z = { fg = colors.purple, bg = colors.gray },
+    a = { fg = colors.purple, bg = colors.grayDark },
+    b = { fg = colors.purple, bg = colors.grayDark },
+    c = { fg = colors.purple, bg = colors.grayDark },
+    x = { fg = colors.purple, bg = colors.grayDark },
+    y = { fg = colors.purple, bg = colors.grayDark },
+    z = { fg = colors.purple, bg = colors.grayDark },
   },
   visual = {
-    a = { fg = colors.orange, bg = colors.gray }, 
-    b = { fg = colors.orange, bg = colors.gray },
-    c = { fg = colors.orange, bg = colors.gray },
-    x = { fg = colors.orange, bg = colors.gray },
-    y = { fg = colors.orange, bg = colors.gray },
-    z = { fg = colors.orange, bg = colors.gray },
+    a = { fg = colors.orange, bg = colors.grayDark },
+    b = { fg = colors.orange, bg = colors.grayDark },
+    c = { fg = colors.orange, bg = colors.grayDark },
+    x = { fg = colors.orange, bg = colors.grayDark },
+    y = { fg = colors.orange, bg = colors.grayDark },
+    z = { fg = colors.orange, bg = colors.grayDark },
   },
   command = {
-    a = { fg = colors.turquoise, bg = colors.gray },
-    b = { fg = colors.turquoise, bg = colors.gray },
-    c = { fg = colors.turquoise, bg = colors.gray },
-    x = { fg = colors.turquoise, bg = colors.gray },
-    y = { fg = colors.turquoise, bg = colors.gray },
-    z = { fg = colors.turquoise, bg = colors.gray },
+    a = { fg = colors.turquoise, bg = colors.grayDark },
+    b = { fg = colors.turquoise, bg = colors.grayDark },
+    c = { fg = colors.turquoise, bg = colors.grayDark },
+    x = { fg = colors.turquoise, bg = colors.grayDark },
+    y = { fg = colors.turquoise, bg = colors.grayDark },
+    z = { fg = colors.turquoise, bg = colors.grayDark },
   },
-	replace = {
+  replace = {
     a = {  },
     b = {  },
     c = {  },
@@ -52,12 +48,13 @@ return {
     y = {  },
     z = {  },
   },
-	inactive = {
-		a = {  },
-    b = {  },
-    c = {  },
-    x = {  },
-    y = {  },
-    z = {  },
-	},
+  inactive = {
+    a = { fg = colors.gray, bg = colors.grayDark },
+    b = { fg = colors.gray, bg = colors.grayDark },
+    c = { fg = colors.gray, bg = colors.grayDark },
+    x = { fg = colors.gray, bg = colors.grayDark },
+    y = { fg = colors.gray, bg = colors.grayDark },
+    z = { fg = colors.gray, bg = colors.grayDark },
+  },
 }
+
