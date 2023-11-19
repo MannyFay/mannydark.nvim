6af461fc8f2ac986ab7f705a3b8bf7ff5c2e226c
Manny <mannythefay@gmail.com>
2023-11-19 18:33:00 +0100

    Tidy up and set up colors to test

--- lua/mannydark/theme.lua
+++ lua/mannydark/theme.lua
@@ -1,90 +1,70 @@
-local c = require('mannydark.palette')
-
-local hl = vim.api.nvim_set_hl
+local c     = require('mannydark.palette')
+local hl    = vim.api.nvim_set_hl
 local theme = {}
 
 theme.set_highlights = function()
-
 ------------------------------------------------------------------------------
 -- Editor
 ------------------------------------------------------------------------------
+
   hl(0, 'FloatBorder', {
       fg = vim.api.nvim_get_hl_by_name('NormalFloat', true).background,
       bg = vim.api.nvim_get_hl_by_name('NormalFloat', true).background,
-  })    
+  })
 
-  --hl(0, 'IndentBlanklineChar', { fg=#252525]])
-      hl(0, 'IndentBlanklineChar', { fg = c.gray_dark, bg = 'NONE' })
-  
-  hl(0, "SignColumn", { fg = 'NONE', bg = c.black })
+  hl(0, 'IndentBlanklineChar', { fg = c.grayDark, bg = 'NONE'               })  -- Indent Blank Line plugin vertical lines.
+  hl(0, 'SignColumn',          { fg = 'NONE',     bg = c.black              })  -- Sign column left of the line numbers.
+  hl(0, 'ColorColumn',         { fg = 'NONE',     bg = c.grayDark           })  -- Vertical visual guide line.
+  hl(0, 'LineNr',              { fg = c.gray,     bg = 'NONE'               })  -- Line numbers.
+  hl(0, 'CursorLineNr',        { fg = c.white,    bg = 'NONE'               })  -- Line number of current line.
+  hl(0, 'VertSplit',           { fg = c.grayDark, bg = c.grayDark           })  -- Vertical split separator line.
+  hl(0, 'CursorLine',          { fg = 'NONE',     bg = c.grayDark           })  -- Color of actual line.
+  hl(0, 'Search',              { fg = c.black,    bg = c.pink               })  -- Highlighting of search patterns.
+  hl(0, 'IncSearch',           { fg = c.black,    bg = c.white              })  -- Highlighting of first result from search pattern.
+  hl(0, 'EndOfBuffer',         { fg = c.black,    bg = 'NONE'               })  -- Empty end of buffer.
+  hl(0, 'ErrorMsg',            { fg = c.red,      bg = 'NONE', bold = true, })  -- Error message.
+  hl(0, 'Visual',              { fg = 'NONE',     bg = c.gray               })
+  hl(0, 'Pmenu',               { fg = c.white,    bg = c.grayDark           })
+  hl(0, 'PmenuSel',            { fg = c.black,    bg = c.gray               })
 
-  -- Vertical visual guide line:
-  hl(0, "ColorColumn", { fg = 'NONE', bg = c.gray_dark })
-
-  hl(0, "LineNr", { fg = c.gray, bg = 'NONE' })
-  hl(0, "CursorLineNr", { fg = c.white, bg = 'NONE' })
-  -- Vertical split separator line:
-  hl(0, "VertSplit", { fg = c.gray_dark, bg = c.gray_dark })
-  -- Color of actual line:
-  hl(0, "CursorLine", { fg = 'NONE', bg = c.gray_dark })
-  -- Highlighting of search patterns:
-  hl(0, "Search", { fg = c.black, bg = c.pink })
-  -- Highlighting of first result from search pattern:
-  hl(0, "IncSearch", { fg = c.black, bg = c.white })
-  -- Empty end of buffer:
-  hl(0, "EndOfBuffer", { fg = c.black, bg = 'NONE' })
-  -- Error message:
-  hl(0, "ErrorMsg", { fg = c.white, bg = c.red, bold = true, })
-  hl(0, "Visual", { fg = 'NONE', bg = c.gray })
-  hl(0, "Pmenu", { fg = c.white, bg = c.gray_dark })
-  hl(0, "PmenuSel", { fg = c.black, bg = c.gray })
-
-----------------------------
+---------------------------- Not used by now:
   hl(0, "Cursor", { fg = c.red, bg = c.green })
   hl(0, "lCursor", { fg = c.red, bg = c.green })
   hl(0, "CursorIM", { fg = c.red, bg = c.green })
-
-  hl(0, "MsgArea", { fg = c.fg, bg = c.bg })
-  hl(0, "ModeMsg", { fg = c.fg, bg = c.alt_bg })
-  hl(0, "MsgSeparator", { fg = c.fg, bg = c.bg })
-  hl(0, "SpellBad", { fg = 'NONE', bg = 'NONE', sp = c.red, undercurl = true, })
-  hl(0, "SpellCap", { fg = 'NONE', bg = 'NONE', sp = c.yellow, undercurl = true, })
-  hl(0, "SpellLocal", { fg = 'NONE', bg = 'NONE', sp = c.green, underline = true, })
-  hl(0, "SpellRare", { fg = 'NONE', bg = 'NONE', sp = c.purple, underline = true, })
-  hl(0, "NormalNC", { fg = c.fg, bg = c.bg })
-
+  hl(0, "MsgArea", { fg = c.gray, bg = c.orange })
+  hl(0, "ModeMsg", { fg = c.green, bg = c.gray })
+  hl(0, "MsgSeparator", { fg = c.blue, bg = c.bg })
+  hl(0, "SpellBad", { fg = c.gray, bg = c.green, sp = c.red, undercurl = true, })
+  hl(0, "SpellCap", { fg = c.orange, bg = 'NONE', sp = c.yellow, undercurl = true, })
+  hl(0, "SpellLocal", { fg = c.blue, bg = c.orange, sp = c.green, underline = true, })
+  hl(0, "SpellRare", { fg = 'NONE', bg = c.green, sp = c.purple, underline = true, })
+  hl(0, "NormalNC", { fg = c.blue, bg = c.bg })
   hl(0, "WildMenu", { fg = c.red, bg = c.purple })
-
-  hl(0, "Folded", { fg = c.gray, bg = c.alt_bg })
-  hl(0, "FoldColumn", { fg = c.gray, bg = c.alt_bg })
+  hl(0, "Folded", { fg = c.gray, bg = c.orange })
+  hl(0, "FoldColumn", { fg = c.gray, bg = c.yellow })
   hl(0, "FloatBorder", { fg = c.gray, bg = c.alt_bg })
-  hl(0, "Whitespace", { fg = c.dark_gray, bg = 'NONE' })
-
+  hl(0, "Whitespace", { fg = c.darkGray, bg = c.yellow })
   hl(0, "CursorColumn", { fg = c.blue, bg = c.red })
-  --hl(0, "CursorColumn", { fg = 'NONE', bg = c.alt_bg })
-
-  hl(0, "NormalFloat", { fg = 'NONE', bg = c.alt_bg })
-  hl(0, "VisualNOS", { fg = 'NONE', bg = c.alt_bg })
-  hl(0, "WarningMsg", { fg = c.ui_orange, bg = c.bg })
-  hl(0, "QuickFixLine", { fg = 'NONE', bg = c.ui2_blue })
-  hl(0, "PmenuSbar", { fg = 'NONE', bg = c.alt_bg })
+  hl(0, "NormalFloat", { fg = c.green, bg = c.blue })
+  hl(0, "VisualNOS", { fg = c.blue, bg = c.orange })
+  hl(0, "WarningMsg", { fg = c.ui_orange, bg = c.blue })
+  hl(0, "QuickFixLine", { fg = c.green, bg = c.orange })
+  hl(0, "PmenuSbar", { fg = c.pink, bg = c.yellow })
   hl(0, "PmenuThumb", { fg = 'NONE', bg = c.gray })
-  hl(0, "MatchWord", { fg = 'NONE', bg = c.reference })
-  hl(0, "MatchWordCur", { fg = 'NONE', bg = c.reference })
-  hl(0, "MatchParenCur", { fg = 'NONE', bg = c.reference })
-  hl(0, "TermCursor", { fg = c.cursor_fg, bg = c.cursor_bg })
-  hl(0, "TermCursorNC", { fg = c.cursor_fg, bg = c.cursor_bg })
-  hl(0, "Conceal", { fg = c.gray, bg = 'NONE' })
-  hl(0, "SpecialKey", { fg = c.blue, bg = 'NONE', bold = true, })
-  hl(0, "Substitute", { fg = 'NONE', bg = c.ui2_orange })
+  hl(0, "MatchWord", { fg = c.pink, bg = c.purple })
+  hl(0, "MatchWordCur", { fg = c.orange, bg = c.reference })
+  hl(0, "MatchParenCur", { fg = c.purple, bg = c.pink })
+  hl(0, "TermCursor", { fg = c.pink, bg = c.purple })
+  hl(0, "TermCursorNC", { fg = c.cursor_fg, bg = c.yellow })
+  hl(0, "Conceal", { fg = c.gray, bg = c.purple })
+  hl(0, "SpecialKey", { fg = c.blue, bg = c.pink, bold = true, })
+  hl(0, "Substitute", { fg = 'NONE', bg = c.purple })
   hl(0, "MoreMsg", { fg = c.orange, bg = 'NONE' })
-  hl(0, "Question", { fg = c.orange, bg = 'NONE' })
+  hl(0, "Question", { fg = c.orange, bg = c.pink })
   hl(0, "NonText", { fg = c.dark_gray, bg = 'NONE' })
-
   hl(0, "TabLine", { fg = c.red, bg = c.green })
-
-  hl(0, "TabLineSel", { fg = c.fg, bg = c.line })
-  hl(0, "TabLineFill", { fg = c.line, bg = c.line })
+  hl(0, "TabLineSel", { fg = c.green, bg = c.orange })
+  hl(0, "TabLineFill", { fg = c.green, bg = c.line })
 
 
 --------------------------------------------------------------
