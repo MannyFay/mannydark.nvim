1929c154687bb63db6237ceabc23bb74efd45b7c
Manny Fay <mannythefay@gmail.com>
2023-11-20 07:23:28 +0100

    Update theme.lua

--- lua/mannydark/theme.lua
+++ lua/mannydark/theme.lua
@@ -13,7 +13,7 @@ theme.set_highlights = function()
   })
 
   hl(0, 'IndentBlanklineChar', { fg = c.grayDark, bg = 'NONE'              })  -- Indent Blank Line plugin vertical lines.
-  hl(0, 'SignColumn',          { fg = 'NONE',     bg = c.green             })  -- Sign column left of the line numbers.
+  hl(0, 'SignColumn',          { fg = 'NONE',     bg = c.black             })  -- Sign column left of the line numbers.
   hl(0, 'ColorColumn',         { fg = 'NONE',     bg = c.grayDark          })  -- Vertical visual guide line.
   hl(0, 'LineNr',              { fg = c.gray,     bg = 'NONE'              })  -- Line numbers.
   hl(0, 'CursorLineNr',        { fg = c.white,    bg = 'NONE'              })  -- Line number of current line.
@@ -34,7 +34,7 @@ theme.set_highlights = function()
   hl(0, 'Whitespace',          { fg = c.red,      bg = 'NONE' }) -- Trailing whitespaces in buffer.
   hl(0, 'PmenuSbar',           { fg = 'NONE',     bg = c.grayDark })  -- Scroll bar background in context menus.
   hl(0, 'PmenuThumb',          { fg = c.white,    bg = 'NONE' }) -- Scroll bar in context menus.
-  hl(0, 'MsgSeparator',        { fg = 'NONE',     bg = c.red }) -- Seperator line above messages under status line.
+  hl(0, 'MsgSeparator',        { fg = 'NONE',     bg = c.black }) -- Seperator line above messages under status line.
   hl(0, 'SpellBad',            { fg = 'NONE',     bg = 'NONE', sp = c.red, undercurl = true }) -- Spelling mistakes.
 
 
