90ab60d98bdaf4223ee0e91fdb7a1876f10ff630
Manny Fay <mannythefay@gmail.com>
2023-11-20 08:11:58 +0100

    Update theme.lua

--- lua/mannydark/theme.lua
+++ lua/mannydark/theme.lua
@@ -103,13 +103,13 @@ theme.set_highlights = function()
 --------------------------------------------------------------
 -- Hop
 
-  hl(0, 'HopCursor',  { fg = c.blue, bg = c.blue              })  -- Cursor in Hop mode.
-  hl(0, 'HopNextKey', { fg = c.blue, bg = 'NONE', bold = true })  -- Neareast keys with one char.
-  hl(0, 'HopNextKey1', { fg = c.purple, bg = 'NONE', bold = true })
-  hl(0, 'HopNextKey2', { fg = c.purple, bg = 'NONE', bold = true })
-
+  hl(0, 'HopCursor',    { fg = c.blue,   bg = c.blue              })  -- Cursor in Hop mode.
+  hl(0, 'HopNextKey',   { fg = c.blue,   bg = 'NONE', bold = true })  -- Neareast keys with one char.
+  hl(0, 'HopNextKey1',  { fg = c.purple, bg = 'NONE', bold = true })  -- 
+  hl(0, 'HopNextKey2',  { fg = c.turquoise, bg = 'NONE', bold = true })
+  hl(0, 'HopUnmatched', { fg = c.gray,   bg = 'NONE'              })  -- Regular not matching text if Hop is active.
   
-  hl(0, "HopUnmatched", { fg = c.orange, bg = c.green })
+  ----------------------- Not used by now:
   hl(0, "HopPreview", { fg = c.pink, bg = c.purple })
 
   
