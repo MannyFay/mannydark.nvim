2a71280aa1527ae52b0cb9fce96f8f3f12c00a89
Manny <mannythefay@gmail.com>
2023-11-25 10:20:25 +0100

    Set up colors

--- lua/mannydark/highlighting/languages/dockercompose.lua
+++ lua/mannydark/highlighting/languages/dockercompose.lua
@@ -11,15 +11,17 @@ local dockerCompose = {}
 -- Settings
 
 dockerCompose.setupHighlighting = function()
+  highlight(0, 'dockercomposeComment',  { fg = colors.red,      bg = 'NONE'           })  -- Comments.
+  highlight(0, 'dockercomposeKeywords', { fg = colors.blue,     bg = 'NONE'           })  -- Keywords.
+  highlight(0, 'dockercomposeString1',  { fg = colors.redLight, bg = 'NONE'           })  -- Strings in single quotes.
+  highlight(0, 'dockercomposeString',   { fg = colors.redLight, bg = 'NONE'           })  -- Strings in double quotes.
+
   ----------------------- Not used by now:
-  highlight(0, 'dockercomposeKeywords', { fg = colors.blue, bg = colors.pink })
-  highlight(0, 'bashStatement',         { fg = colors.blue, bg = colors.pink })
-  highlight(0, 'dockercomposeString',   { fg = colors.blue, bg = colors.pink })
-  highlight(0, 'dockercomposeString1',  { fg = colors.blue, bg = colors.pink })
-  highlight(0, 'dockercomposeEmail',    { fg = colors.blue, bg = colors.pink })
-  highlight(0, 'dockercomposeUrl',      { fg = colors.blue, bg = colors.pink })
-  highlight(0, 'dockercomposeTodo',     { fg = colors.blue, bg = colors.pink })
-  highlight(0, 'dockercomposeComment',  { fg = colors.blue, bg = colors.pink })
+  highlight(0, 'bashStatement',         { fg = colors.blue,     bg = colors.gray      })
+  highlight(0, 'dockercomposeEmail',    { fg = colors.blue,     bg = colors.orange    })
+  highlight(0, 'dockercomposeUrl',      { fg = colors.blue,     bg = colors.purple    })
+  highlight(0, 'dockercomposeTodo',     { fg = colors.blue,     bg = colors.turquoise })
 end
 
 return dockerCompose
+
