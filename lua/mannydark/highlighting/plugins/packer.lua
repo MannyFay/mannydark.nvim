c56b3ee0a0b15a01e6805f65dffd03d592e4b502
Manny <mannythefay@gmail.com>
2023-11-26 12:23:50 +0100

    Update Packers floating window highlighting

--- lua/mannydark/highlighting/plugins/packer.lua
+++ lua/mannydark/highlighting/plugins/packer.lua
@@ -11,13 +11,15 @@ local packer    = {}
 -- Settings
 
 packer.setupHighlighting = function()
+  highlight(0, 'packerSuccess',       { fg = colors.green,    bg = 'NONE' })  -- Left sign column.
+  highlight(0, 'packerOutput',        { fg = colors.blue,     bg = 'NONE' })  -- Topic of Packers doings.
+  highlight(0, 'packerHash',          { fg = colors.green,    bg = 'NONE' })  -- Message of Packers doings.
+  highlight(0, 'packerStatusSuccess', { fg = colors.green,    bg = 'NONE' })  -- Success message.
+  highlight(0, 'packerString',        { fg = colors.redLight, bg = 'NONE' })  -- Message what user can do.
+
   ----------------------- Not used by now:
-  highlight(0, 'packerString',        { fg = colors.red, bg = colors.green })
-  highlight(0, 'packerHash',          { fg = colors.red, bg = colors.green })
-  highlight(0, 'packerOutput',        { fg = colors.red, bg = colors.green })
-  highlight(0, 'packerRelDate',       { fg = colors.red, bg = colors.green })
-  highlight(0, 'packerSuccess',       { fg = colors.red, bg = colors.green })
-  highlight(0, 'packerStatusSuccess', { fg = colors.red, bg = colors.green })
+  highlight(0, 'packerRelDate',       { fg = colors.red, bg = colors.blue })
 end
 
 return packer
+
