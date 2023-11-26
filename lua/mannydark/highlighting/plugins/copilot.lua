df16410b0b779167693e905686a7ff37def81f93
Manny <mannythefay@gmail.com>
2023-11-26 12:20:21 +0100

    Add Copilot to highlight groups

---
+++ lua/mannydark/highlighting/plugins/copilot.lua
@@ -0,0 +1,18 @@
+------------------------------------------------------------------------------
+-- Copilot Neovim Plugin
+------------------------------------------------------------------------------
+
+local colors    = require('mannydark.palette')
+local highlight = vim.api.nvim_set_hl
+local copilot   = {}
+
+
+--------------------------------------------------------------
+-- Settings
+
+copilot.setupHighlighting = function()
+  highlight(0, 'CopilotSuggestion', { fg = colors.gray, bg = 'NONE' })  -- Provided suggestions.
+end
+
+return copilot
+
--- lua/mannydark/theme.lua
+++ lua/mannydark/theme.lua
@@ -20,6 +20,7 @@ theme.buildTheme = function()
   require('mannydark.highlighting.languages.xml'             ).setupHighlighting()
   require('mannydark.highlighting.languages.yml'             ).setupHighlighting()
   -- Plugins:
+  require('mannydark.highlighting.plugins.copilot'           ).setupHighlighting()
   require('mannydark.highlighting.plugins.diffview'          ).setupHighlighting()
   require('mannydark.highlighting.plugins.gitsigns'          ).setupHighlighting()
   require('mannydark.highlighting.plugins.hop'               ).setupHighlighting()
