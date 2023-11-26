440189a674b526a0f6a2f9a2ed57bb1e67e27b4b
Manny <mannythefay@gmail.com>
2023-11-26 12:22:29 +0100

    Update highlighting that it works globally

--- lua/mannydark/highlighting/plugins/lualine.lua
+++ lua/mannydark/highlighting/plugins/lualine.lua
@@ -12,41 +12,56 @@ local lualinePlugin = {}
 
 lualinePlugin.setupHighlighting = function()
   -- Normal Mode:
-  highlight(0, 'lualine_a_normal', { fg = colors.blue, bg = colors.grayDark })
-  highlight(0, 'lualine_b_normal', { fg = colors.blue, bg = colors.grayDark })
-  highlight(0, 'lualine_c_normal', { fg = colors.blue, bg = colors.grayDark })
-  highlight(0, 'lualine_x_normal', { fg = colors.blue, bg = colors.grayDark })
-  highlight(0, 'lualine_y_normal', { fg = colors.blue, bg = colors.grayDark })
-  highlight(0, 'lualine_z_normal', { fg = colors.blue, bg = colors.grayDark })
+  highlight(0, 'lualine_a_normal',   { fg = colors.blue,      bg = colors.grayDark })
+  highlight(0, 'lualine_b_normal',   { fg = colors.blue,      bg = colors.grayDark })
+  highlight(0, 'lualine_c_normal',   { fg = colors.blue,      bg = colors.grayDark })
+  highlight(0, 'lualine_x_normal',   { fg = colors.blue,      bg = colors.grayDark })
+  highlight(0, 'lualine_y_normal',   { fg = colors.blue,      bg = colors.grayDark })
+  highlight(0, 'lualine_z_normal',   { fg = colors.blue,      bg = colors.grayDark })
 
-  ----------------------- Not used by now:
   -- Insert Mode:
-  highlight(0, 'lualine_a_insert', { fg = colors.blue, bg = colors.gray    })
-  highlight(0, 'lualine_b_insert', { fg = colors.red,  bg = colors.green   })
+  highlight(0, 'lualine_a_insert',   { fg = colors.purple,    bg = colors.grayDark })
+  highlight(0, 'lualine_b_insert',   { fg = colors.purple,    bg = colors.grayDark })
+  highlight(0, 'lualine_c_insert',   { fg = colors.purple,    bg = colors.grayDark })
+  highlight(0, 'lualine_x_insert',   { fg = colors.purple,    bg = colors.grayDark })
+  highlight(0, 'lualine_y_insert',   { fg = colors.purple,    bg = colors.grayDark })
+  highlight(0, 'lualine_z_insert',   { fg = colors.purple,    bg = colors.grayDark })
 
   -- Visual Mode:
-  highlight(0, 'lualine_a_visual', { fg = colors.red, bg = colors.green })
-  highlight(0, 'lualine_b_visual', { fg = colors.red, bg = colors.green })
+  highlight(0, 'lualine_a_visual',   { fg = colors.orange,    bg = colors.grayDark })
+  highlight(0, 'lualine_b_visual',   { fg = colors.orange,    bg = colors.grayDark })
+  highlight(0, 'lualine_c_visual',   { fg = colors.orange,    bg = colors.grayDark })
+  highlight(0, 'lualine_x_visual',   { fg = colors.orange,    bg = colors.grayDark })
+  highlight(0, 'lualine_y_visual',   { fg = colors.orange,    bg = colors.grayDark })
+  highlight(0, 'lualine_z_visual',   { fg = colors.orange,    bg = colors.grayDark })
 
   -- Command Mode:
-  highlight(0, 'lualine_a_command', { fg = colors.red, bg = colors.green })
-  highlight(0, 'lualine_b_command', { fg = colors.red, bg = colors.green })
-
-  -- Replace Mode:
-  highlight(0, 'lualine_a_replace', { fg = colors.red, bg = colors.green })
-  highlight(0, 'lualine_b_replace', { fg = colors.red, bg = colors.green })
+  highlight(0, 'lualine_a_command',  { fg = colors.turquoise, bg = colors.grayDark })
+  highlight(0, 'lualine_b_command',  { fg = colors.turquoise, bg = colors.grayDark })
+  highlight(0, 'lualine_c_command',  { fg = colors.turquoise, bg = colors.grayDark })
+  highlight(0, 'lualine_x_command',  { fg = colors.turquoise, bg = colors.grayDark })
+  highlight(0, 'lualine_y_command',  { fg = colors.turquoise, bg = colors.grayDark })
+  highlight(0, 'lualine_z_command',  { fg = colors.turquoise, bg = colors.grayDark })
 
   -- Inactive Mode:
-  highlight(0, 'lualine_a_inactive', { fg = colors.red, bg = colors.green })
-  highlight(0, 'lualine_b_inactive', { fg = colors.red, bg = colors.green })
-  highlight(0, 'lualine_c_inactive', { fg = colors.red, bg = colors.green })
+  highlight(0, 'lualine_a_inactive', { fg = colors.gray,      bg = colors.grayDark })
+  highlight(0, 'lualine_b_inactive', { fg = colors.gray,      bg = colors.grayDark })
+  highlight(0, 'lualine_c_inactive', { fg = colors.gray,      bg = colors.grayDark })
+  highlight(0, 'lualine_x_inactive', { fg = colors.gray,      bg = colors.grayDark })
+  highlight(0, 'lualine_y_inactive', { fg = colors.gray,      bg = colors.grayDark })
+  highlight(0, 'lualine_z_inactive', { fg = colors.gray,      bg = colors.grayDark })
 
-  highlight(0, 'lualine_x_diff_added_normal',      { fg = colors.red, bg = colors.green })
-  highlight(0, 'lualine_x_diff_added_insert',      { fg = colors.red, bg = colors.green })
-  highlight(0, 'lualine_x_diff_added_visual',      { fg = colors.red, bg = colors.green })
-  highlight(0, 'lualine_x_diff_added_replace',     { fg = colors.red, bg = colors.green })
-  highlight(0, 'lualine_x_diff_added_command',     { fg = colors.red, bg = colors.green })
-  highlight(0, 'lualine_x_diff_added_terminal',    { fg = colors.red, bg = colors.green })
+  ----------------------- Not used by now:
+  -- Replace Mode:
+  highlight(0, 'lualine_a_replace', { fg = colors.red, bg = colors.greenLight })
+  highlight(0, 'lualine_b_replace', { fg = colors.red, bg = colors.green })
+
+  highlight(0, 'lualine_x_diff_added_normal',      { fg = colors.green, bg = colors.redLight })
+  highlight(0, 'lualine_x_diff_added_insert',      { fg = colors.green, bg = colors.orange })
+  highlight(0, 'lualine_x_diff_added_visual',      { fg = colors.green, bg = colors.pink })
+  highlight(0, 'lualine_x_diff_added_replace',     { fg = colors.green, bg = colors.blue })
+  highlight(0, 'lualine_x_diff_added_command',     { fg = colors.green, bg = colors.greenLight })
+  highlight(0, 'lualine_x_diff_added_terminal',    { fg = colors.green, bg = colors.turquoise })
   highlight(0, 'lualine_x_diff_added_inactive',    { fg = colors.red, bg = colors.green })
   highlight(0, 'lualine_x_diff_modified_normal',   { fg = colors.red, bg = colors.green })
   highlight(0, 'lualine_x_diff_modified_insert',   { fg = colors.red, bg = colors.green })
@@ -65,3 +80,4 @@ lualinePlugin.setupHighlighting = function()
 end
 
 return lualinePlugin
+
