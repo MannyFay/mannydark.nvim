e693fe0c69815ec772e2939e53f434b92a051cb2
Manny <mannythefay@gmail.com>
2023-11-26 12:23:13 +0100

    Update completion menu highlighting

--- lua/mannydark/highlighting/plugins/nvimcmp.lua
+++ lua/mannydark/highlighting/plugins/nvimcmp.lua
@@ -11,36 +11,37 @@ local nvimCmp   = {}
 -- Settings
 
 nvimCmp.setupHighlighting = function()
-  highlight(0, 'CmpItemAbbrMatch',         { fg = colors.black, bg = colors.gray                       })
-  highlight(0, 'CmpItemAbbrDeprecated',    { fg = colors.gray,  bg = 'NONE',      strikethrough = true })
+  highlight(0, 'CmpItemAbbrMatch',         { fg = colors.black,     bg = colors.gray                       })
+  highlight(0, 'CmpItemAbbrDeprecated',    { fg = colors.gray,      bg = 'NONE',      strikethrough = true })
+  highlight(0, 'CmpItemKindSnippet',       { fg = colors.green,     bg = 'NONE'                            })  -- Snippet kind marker text.
+  highlight(0, 'CmpItemKindText',          { fg = colors.white,     bg = 'NONE'                            })  -- Text kind marker text.
+  highlight(0, 'CmpItemKindFunction',      { fg = colors.orange,    bg = 'NONE'                            }) -- Function kind marker text.
+  highlight(0, 'CmpItemKindKeyword',       { fg = colors.blue,      bg = 'NONE'                            }) -- Keyword kind marker text.
+  highlight(0, 'CmpItemKindVariable',      { fg = colors.purple,    bg = 'NONE'                            }) -- Variable kind marker text.
+  highlight(0, 'CmpItemKindClass',         { fg = colors.turquoise, bg = 'NONE'                            }) -- Class kind marker text.
 
   ----------------------- Not used by now:
-  highlight(0, 'CmpItemAbbrMatchFuzzy',    { fg = colors.green, bg = colors.orange                     })
-  highlight(0, 'CmpItemKindFunction',      { fg = colors.green, bg = colors.orange                     })
-  highlight(0, 'CmpItemKindMethod',        { fg = colors.green, bg = colors.orange                     })
-  highlight(0, 'CmpItemKindConstructor',   { fg = colors.green, bg = colors.orange                     })
-  highlight(0, 'CmpItemKindClass',         { fg = colors.green, bg = colors.orange                     })
-  highlight(0, 'CmpItemKindEnum',          { fg = colors.green, bg = colors.orange                     })
-  highlight(0, 'CmpItemKindEvent',         { fg = colors.green, bg = colors.orange                     })
-  highlight(0, 'CmpItemKindInterface',     { fg = colors.green, bg = colors.orange                     })
-  highlight(0, 'CmpItemKindStruct',        { fg = colors.green, bg = colors.orange                     })
-  highlight(0, 'CmpItemKindVariable',      { fg = colors.green, bg = colors.orange                     })
-  highlight(0, 'CmpItemKindField',         { fg = colors.green, bg = colors.orange                     })
-  highlight(0, 'CmpItemKindProperty',      { fg = colors.green, bg = colors.orange                     })
-  highlight(0, 'CmpItemKindEnumMember',    { fg = colors.green, bg = colors.orange                     })
-  highlight(0, 'CmpItemKindConstant',      { fg = colors.green, bg = colors.orange                     })
-  highlight(0, 'CmpItemKindKeyword',       { fg = colors.green, bg = colors.orange                     })
-  highlight(0, 'CmpItemKindModule',        { fg = colors.green, bg = colors.orange                     })
-  highlight(0, 'CmpItemKindValue',         { fg = colors.green, bg = colors.orange                     })
-  highlight(0, 'CmpItemKindUnit',          { fg = colors.green, bg = colors.orange                     })
-  highlight(0, 'CmpItemKindText',          { fg = colors.green, bg = colors.orange                     })
-  highlight(0, 'CmpItemKindSnippet',       { fg = colors.green, bg = colors.orange                     })
-  highlight(0, 'CmpItemKindFile',          { fg = colors.green, bg = colors.orange                     })
-  highlight(0, 'CmpItemKindFolder',        { fg = colors.green, bg = colors.orange                     })
-  highlight(0, 'CmpItemKindColor',         { fg = colors.green, bg = colors.orange                     })
-  highlight(0, 'CmpItemKindReference',     { fg = colors.green, bg = colors.orange                     })
-  highlight(0, 'CmpItemKindOperator',      { fg = colors.green, bg = colors.orange                     })
-  highlight(0, 'CmpItemKindTypeParameter', { fg = colors.green, bg = colors.orange                     })
+  highlight(0, 'CmpItemAbbrMatchFuzzy',    { fg = colors.green, bg = colors.orange                         })
+  highlight(0, 'CmpItemKindMethod',        { fg = colors.green, bg = colors.redLight                       })
+  highlight(0, 'CmpItemKindConstructor',   { fg = colors.green, bg = colors.blue                           })
+  highlight(0, 'CmpItemKindEnum',          { fg = colors.green, bg = colors.ping                           })
+  highlight(0, 'CmpItemKindEvent',         { fg = colors.green, bg = colors.turquoise                      })
+  highlight(0, 'CmpItemKindInterface',     { fg = colors.green, bg = colors.greenLight                     })
+  highlight(0, 'CmpItemKindStruct',        { fg = colors.green, bg = colors.gray                           })
+  highlight(0, 'CmpItemKindField',         { fg = colors.green, bg = colors.purple                         })
+  highlight(0, 'CmpItemKindProperty',      { fg = colors.pink, bg = colors.blue                            })
+  highlight(0, 'CmpItemKindEnumMember',    { fg = colors.pink, bg = colors.green                           })
+  highlight(0, 'CmpItemKindConstant',      { fg = colors.pink, bg = colors.greenLight                      })
+  highlight(0, 'CmpItemKindModule',        { fg = colors.pink, bg = colors.redLight                        })
+  highlight(0, 'CmpItemKindValue',         { fg = colors.pink, bg = colors.turquoise                       })
+  highlight(0, 'CmpItemKindUnit',          { fg = colors.pink, bg = colors.purple                          })
+  highlight(0, 'CmpItemKindFile',          { fg = colors.blue, bg = colors.redLight                        })
+  highlight(0, 'CmpItemKindFolder',        { fg = colors.blue, bg = colors.green                           })
+  highlight(0, 'CmpItemKindColor',         { fg = colors.blue, bg = colors.greenLight                      })
+  highlight(0, 'CmpItemKindReference',     { fg = colors.blue, bg = colors.purple                          })
+  highlight(0, 'CmpItemKindOperator',      { fg = colors.green, bg = colors.pink                           })
+  highlight(0, 'CmpItemKindTypeParameter', { fg = colors.green, bg = colors.turquoise                      })
 end
 
 return nvimCmp
+
