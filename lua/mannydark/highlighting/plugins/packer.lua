f7973f398961386a4bc8d62f245e0b9ded79f6f3
Manny <mannythefay@gmail.com>
2023-11-27 23:21:41 +0100

    Update Packer highlighting

--- lua/mannydark/highlighting/plugins/packer.lua
+++ lua/mannydark/highlighting/plugins/packer.lua
@@ -12,13 +12,11 @@ local packer    = {}
 
 packer.setupHighlighting = function()
   highlight(0, 'packerSuccess',       { fg = colors.green,    bg = 'NONE' })  -- Left sign column.
-  highlight(0, 'packerOutput',        { fg = colors.blue,     bg = 'NONE' })  -- Topic of Packers doings.
-  highlight(0, 'packerHash',          { fg = colors.green,    bg = 'NONE' })  -- Message of Packers doings.
+  highlight(0, 'packerOutput',        { fg = colors.green,    bg = 'NONE' })  -- Topic of Packers doings.
+  highlight(0, 'packerHash',          { fg = colors.white,    bg = 'NONE' })  -- Message of Packers doings.
   highlight(0, 'packerStatusSuccess', { fg = colors.green,    bg = 'NONE' })  -- Success message.
   highlight(0, 'packerString',        { fg = colors.redLight, bg = 'NONE' })  -- Message what user can do.
-
-  ----------------------- Not used by now:
-  highlight(0, 'packerRelDate',       { fg = colors.red, bg = colors.blue })
+  highlight(0, 'packerRelDate',       { fg = colors.white,    bg = 'NONE' })  -- Timestamp of commit release.
 end
 
 return packer
