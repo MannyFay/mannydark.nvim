83fe5dd34b73cd5e776d5a53649ac2f523e40c60
MannyFay <mannythefay@gmail.com>
2024-02-08 09:19:11 +0100

    Start to set up Telescope highlighting

--- lua/mannydark/highlighting/plugins/telescope.lua
+++ lua/mannydark/highlighting/plugins/telescope.lua
@@ -11,13 +11,14 @@ local telescope = {}
 -- Settings
 
 telescope.setupHighlighting = function()
+  highlight(0, 'TelescopePromptPrefix',   { fg = colors.white, bg = 'NONE'                              })  -- Magnifying glass.
+  highlight(0, 'TelescopeMatching',       { fg = colors.black, bg = colors.pink                         })  -- Matched text in autocomplete menu.
+
   ----------------------- Not used by now:
   highlight(0, 'TelescopeSelection',      { fg = colors.blue, bg = colors.pink                              })
   highlight(0, 'TelescopeSelectionCaret', { fg = colors.red,  bg = colors.pink                              })
-  highlight(0, 'TelescopeMatching',       { fg = colors.blue, bg = colors.red, bold = true, italic = true, })
   highlight(0, 'TelescopeBorder',         { fg = colors.blue, bg = colors.orange                              })
   highlight(0, 'TelescopeNormal',         { fg = colors.blue, bg = colors.green                              })
-  highlight(0, 'TelescopePromptPrefix',   { fg = colors.blue, bg = colors.redLight                              })
   highlight(0, 'TelescopePromptTitle',    { fg = colors.blue, bg = colors.turquoise, bold = true,                })
   highlight(0, 'TelescopeResultsTitle',   { fg = colors.blue, bg = colors.purple, bold = true,                })
   highlight(0, 'TelescopePreviewTitle',   { fg = colors.blue, bg = colors.greenLight, bold = true,                })
