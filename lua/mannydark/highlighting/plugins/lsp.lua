94027495069f03ed75a39c8e5acca931d25fc490
MannyFay <mannythefay@gmail.com>
2024-02-25 19:19:29 +0100

    Test for error highlighting

--- lua/mannydark/highlighting/plugins/lsp.lua
+++ lua/mannydark/highlighting/plugins/lsp.lua
@@ -18,7 +18,7 @@ lsp.setupHighlighting = function()
   highlight(0, 'DiagnosticVirtualTextHint',           { fg = colors.blue,   bg = 'NONE', italic = true                         })  -- Horizontal hint indicator bar and text at end of line.
   highlight(0, 'DiagnosticVirtualTextInfo',           { fg = colors.blue,   bg = 'NONE', italic = true                         })  -- Horizontal info indicator bar and text at end of line.
   highlight(0, 'DiagnosticVirtualTextWarn',           { fg = colors.orange, bg = 'NONE', italic = true                         })  -- Horizontal warning indicator bar and text at end of line.
-  highlight(0, 'DiagnosticVirtualTextError',          { fg = colors.red,    bg = 'NONE', italic = true                         })  -- Horizontal error indicator bar and text at end of line.
+  highlight(0, 'DiagnosticVirtualTextError',          { fg = colors.red,    bg = 'NONE'                         })  -- Horizontal error indicator bar and text at end of line.
   highlight(0, 'LspDiagnosticsError',                 { fg = colors.red,    bg = 'NONE'                                        })
   highlight(0, 'LspDiagnosticsWarning',               { fg = colors.orange, bg = 'NONE'                                        })
   highlight(0, 'LspDiagnosticsInfo',                  { fg = colors.blue,   bg = 'NONE'                                        })
