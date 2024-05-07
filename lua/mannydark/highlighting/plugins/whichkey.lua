4246424c639d46838fe8b118c8199ae93647368e
MannyFay <mannythefay@gmail.com>
2024-05-07 07:02:25 +0200

    Update Which-Key

--- lua/mannydark/highlighting/plugins/whichkey.lua
+++ lua/mannydark/highlighting/plugins/whichkey.lua
@@ -11,12 +11,13 @@ local whichKey  = {}
 -- Settings
 
 whichKey.setupHighlighting = function()
-  ----------------------- Not used by now:
-  highlight(0, 'WhichKey',          { fg = colors.purple, bg = colors.redLight })
-  highlight(0, 'WhichKeySeperator', { fg = colors.purple, bg = colors.redLight })
-  highlight(0, 'WhichKeyGroup',     { fg = colors.purple, bg = colors.redLight })
-  highlight(0, 'WhichKeyDesc',      { fg = colors.purple, bg = colors.redLight })
-  highlight(0, 'WhichKeyFloat',     { fg = colors.purple, bg = colors.redLight })
+  highlight(0, 'WhichKey',          { fg = colors.blue, bg = colors.grayDark })
+  highlight(0, 'WhichKeyGroup',     { fg = colors.purple, bg = colors.grayDark })
+  highlight(0, 'WhichKeySeperator', { fg = colors.white, bg = colors.grayDark })
+  highlight(0, 'WhichKeyDesc',      { fg = colors.purple, bg = colors.grayDark })
+  highlight(0, 'WhichKeyFloat',     { fg = colors.purple, bg = colors.grayDark })
+  highlight(0, 'WhichKeyBorder',    { fg = colors.purple, bg = colors.grayDark })
+  highlight(0, 'WhichKeyValue',     { fg = colors.purple, bg = colors.grayDark })
 end
 
 return whichKey
