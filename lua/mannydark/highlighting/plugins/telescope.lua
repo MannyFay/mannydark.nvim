8126747831c9a48632a1ef7f837f276e0db69dc0
MannyFay <mannythefay@gmail.com>
2024-05-07 07:53:46 +0200

    Update Telescope

--- lua/mannydark/highlighting/plugins/telescope.lua
+++ lua/mannydark/highlighting/plugins/telescope.lua
@@ -1,5 +1,6 @@
 -------------------------------------------------------------------------------
 -- Telescope Neovim Plugin
+-- -- https://github.com/nvim-telescope/telescope.nvim
 -------------------------------------------------------------------------------
 
 local colors    = require('mannydark.palette')
@@ -11,19 +12,22 @@ local telescope = {}
 -- Settings
 
 telescope.setupHighlighting = function()
-  highlight(0, 'TelescopePromptPrefix',   { fg = colors.white, bg = 'NONE'                              })  -- Magnifying glass.
-  highlight(0, 'TelescopeMatching',       { fg = colors.black, bg = colors.pink                         })  -- Matched text in autocomplete menu.
+  highlight(0, "TelescopePromptTitle",   { fg = colors.white, bg = colors.grayDark      })
+  highlight(0, "TelescopeResultsTitle",  { fg = colors.white, bg = colors.grayDark      })
+  highlight(0, "TelescopePreviewTitle",  { fg = colors.white, bg = colors.grayDark      })
+  highlight(0, "TelescopeNormal",        { fg = colors.white, bg = colors.black         })
+  highlight(0, "TelescopeBorder",        { fg = colors.white, bg = colors.grayDark      })
+  highlight(0, "TelescopePromptBorder",  { fg = colors.grayDark, bg = colors.grayDark   })
+  highlight(0, "TelescopeResultsBorder", { fg = colors.grayDark, bg = colors.grayDark   })
+  highlight(0, "TelescopePreviewBorder", { fg = colors.grayDark, bg = colors.grayDark   })
+  highlight(0, "TelescopeSelection",     { fg = colors.white, bg = colors.grayDark      })
+  highlight(0, "TelescopePromptCounter", { fg = colors.greenLight, bg = colors.grayDark })
+  highlight(0, "TelescopePromptPrefix",  { fg = colors.white, bg = 'NONE'               })  -- Magnifying glass.
+  highlight(0, "TelescopeMatching",      { fg = colors.black, bg = colors.blue          })  -- Matched text in autocomplete menu.
 
   ----------------------- Not used by now:
-  highlight(0, 'TelescopeSelection',      { fg = colors.blue, bg = colors.pink                          })
-  highlight(0, 'TelescopeSelectionCaret', { fg = colors.red,  bg = colors.pink                          })
-  highlight(0, 'TelescopeBorder',         { fg = colors.blue, bg = colors.orange                        })
-  highlight(0, 'TelescopeNormal',         { fg = colors.blue, bg = colors.green                         })
-  highlight(0, 'TelescopePromptTitle',    { fg = colors.blue, bg = colors.turquoise, bold = true,       })
-  highlight(0, 'TelescopeResultsTitle',   { fg = colors.blue, bg = colors.purple, bold = true,          })
-  highlight(0, 'TelescopePreviewTitle',   { fg = colors.blue, bg = colors.greenLight, bold = true,      })
-  highlight(0, 'TelescopePromptCounter',  { fg = colors.blue, bg = colors.white                         })
-  highlight(0, 'TelescopePreviewHyphen',  { fg = colors.green, bg = colors.pink                         })
+  highlight(0, "TelescopeSelectionCaret", { fg = colors.red,  bg = colors.pink          })
+  highlight(0, "TelescopePreviewHyphen",  { fg = colors.green, bg = colors.pink         })
 end
 
 return telescope
