0f1af10aff1d4609d7ef2200cf7bfb5ef712e2e8
MannyFay <mannythefay@gmail.com>
2024-06-03 22:10:15 +0200

    Update blade

--- lua/mannydark/highlighting/languages/laravelblade.lua
+++ lua/mannydark/highlighting/languages/laravelblade.lua
@@ -3,23 +3,21 @@
 -- Highlighting for Laravel Blade files (*.blade.php).
 -------------------------------------------------------------------------------
 
-local colors       = require('mannydark.palette')
-local highlight    = vim.api.nvim_set_hl
+local colors = require("mannydark.palette")
+local highlight = vim.api.nvim_set_hl
 local laravelBlade = {}
-
 
 --------------------------------------------------------------
 -- Settings
 
 laravelBlade.setupHighlighting = function()
-  highlight(0, 'bladeDelimiter',     { fg = colors.white,  bg = 'NONE' })
-  highlight(0, 'bladeEcho',          { fg = colors.orange, bg = 'NONE' })
-  highlight(0, 'bladePhpParenBlock', { fg = colors.pink,   bg = 'NONE' })
-  highlight(0, 'bladeTodo',          { fg = colors.red,    bg = 'NONE' })
-  highlight(0, 'bladeComment',       { fg = colors.red,    bg = 'NONE' })
-  highlight(0, 'bladeKeyword',       { fg = colors.blue,   bg = 'NONE' })
-  highlight(0, 'bladePhpRegion',     { fg = colors.green,  bg = 'NONE' })
+	highlight(0, "bladeDelimiter", { fg = colors.white, bg = "NONE" })
+	highlight(0, "bladeEcho", { fg = colors.orange, bg = "NONE" })
+	highlight(0, "bladePhpParenBlock", { fg = colors.white, bg = "NONE" })
+	highlight(0, "bladeTodo", { fg = colors.red, bg = "NONE" })
+	highlight(0, "bladeComment", { fg = colors.red, bg = "NONE" })
+	highlight(0, "bladeKeyword", { fg = colors.blue, bg = "NONE" })
+	highlight(0, "bladePhpRegion", { fg = colors.green, bg = "NONE" })
 end
 
 return laravelBlade
-
