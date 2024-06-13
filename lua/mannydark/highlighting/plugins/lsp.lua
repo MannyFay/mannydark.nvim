9388d593d9b7cb629a07bd507d4333c244af645b
MannyFay <mannythefay@gmail.com>
2024-06-14 00:07:58 +0200

    Update JS environment.

--- lua/mannydark/highlighting/plugins/lsp.lua
+++ lua/mannydark/highlighting/plugins/lsp.lua
@@ -11,14 +11,18 @@ local lsp       = {}
 -- Settings
 
 lsp.setupHighlighting = function()
-  highlight(0, 'DiagnosticSignError',                 { fg = colors.red,    bg = 'NONE'                                        })  -- Error icon in sign column.
-  highlight(0, 'DiagnosticUnderlineError',            { fg = 'NONE',        bg = 'NONE', sp = colors.red,    undercurl = true })
-  highlight(0, 'DiagnosticVirtualTextError',          { fg = colors.red,    bg = 'NONE', sp = colors.red,    underline = true })  -- Horizontal error indicator bar and text at end of line.
+  highlight(0, 'DiagnosticSignError',                 { fg = colors.red,    bg = 'NONE'                                        })  -- Error icon (x) in sign column.
+  highlight(0, 'DiagnosticUnderlineError',            { fg = 'NONE',        bg = 'NONE', sp = colors.red,    undercurl = true })  -- Error highlighting in project tree on words.
+  highlight(0, 'DiagnosticVirtualTextError',          { fg = colors.red,    bg = 'NONE', sp = colors.red,    underline = true })  -- Virtual text at the end of the line.
 
-  highlight(0, 'DiagnosticError',                     { fg = colors.red,    bg = colors.turquoise                              })  -- ??? Code error indicator bar and text at end of line.
+  highlight(0, 'DiagnosticError',                     { fg = colors.purple,    bg = colors.turquoise                              })  -- ??? Code error indicator bar and text at end of line.
 
   -- warnings
-  highlight(0, 'DiagnosticWarn',                      { fg = colors.orange, bg = 'NONE'                                        })
+  highlight(0, 'DiagnosticSignWarn',                  { fg = colors.orange, bg = 'NONE'                                       })  -- Warning icon in sign column.
+  highlight(0, 'DiagnosticUnderlineWarn',             { fg = 'NONE',        bg = 'NONE', sp = colors.orange, undercurl = true })  -- Warning highlighting in project tree on words.
+  highlight(0, 'DiagnosticVirtualTextWarn',           { fg = colors.orange, bg = 'NONE', sp = colors.orange, underline = true })  -- Virtual text at the end of the line.
+
+  highlight(0, 'DiagnosticWarn',                      { fg = colors.turquoise, bg = colors.orange                                       })
 
   -- hints are more important than information!
   highlight(0, 'DiagnosticSignHint',                  { fg = colors.green, bg = 'NONE'                                      })  -- Hint icon (bulb) in sign column.
@@ -29,23 +33,22 @@ lsp.setupHighlighting = function()
   highlight(0, 'DiagnosticHint',                      { fg = colors.purple,   bg = colors.blueLink                                       })  -- ??? idk - this is not true -> Code hint indicator bar and text at end of line.
 
   ----- infos
+  highlight(0, 'DiagnosticSignInfo',                  { fg = colors.gray,   bg = 'NONE'                                       })  -- Info icon in sign column.
+  highlight(0, 'DiagnosticUnderlineInfo',             { fg = 'NONE',        bg = 'NONE', sp = colors.gray,   underline = true })  -- Info highlighting in project tree on words.
+  highlight(0, 'DiagnosticVirtualTextInfo',           { fg = colors.gray,   bg = 'NONE', sp = colors.gray,   underline = true })  -- Horizontal info indicator bar and text at end of line.
+
+
   highlight(0, 'DiagnosticInfo',                      { fg = colors.blue,   bg = 'NONE'                                        })
 
 
 
 
-  highlight(0, 'DiagnosticVirtualTextInfo',           { fg = colors.blue,   bg = 'NONE'                                        })  -- Horizontal info indicator bar and text at end of line.
-  highlight(0, 'DiagnosticVirtualTextWarn',           { fg = colors.orange, bg = 'NONE'                                        })  -- Horizontal warning indicator bar and text at end of line.
   highlight(0, 'LspDiagnosticsError',                 { fg = colors.green,    bg = 'NONE'                                        })
   highlight(0, 'LspDiagnosticsWarning',               { fg = colors.orange, bg = 'NONE'                                        })
   highlight(0, 'LspDiagnosticsInfo',                  { fg = colors.blue,   bg = 'NONE'                                        })
   highlight(0, 'LspCodeLens',                         { fg = colors.purple, bg = 'NONE', italic = true,                        })
   highlight(0, 'LspCodeLensSeparator',                { fg = colors.purple, bg = 'NONE', italic = true,                        })
-  highlight(0, 'DiagnosticUnderlineInfo',             { fg = 'NONE',        bg = 'NONE', sp = colors.blue,   underline = true, })
-  highlight(0, 'DiagnosticUnderlineWarn',             { fg = 'NONE',        bg = 'NONE', sp = colors.orange, undercurl = true, })
 
-  highlight(0, 'DiagnosticSignWarn',                  { fg = colors.orange, bg = 'NONE'                                        })  -- Warning icon in sign column.
-  highlight(0, 'DiagnosticSignInfo',                  { fg = colors.blue,   bg = 'NONE'                                        })  -- Info icon in sign column. -- not true! its yellow...
 
   highlight(0, 'LspReferenceRead',                     { fg = "NONE", bg = colors.gray                                    })  -- Matching words to the one under cursor, like HTML tag names.
 
