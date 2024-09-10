2e39cefc50028b3e011c9d1044666d617804cb7b
Manuel-Hubertus Fay <MannyFay@users.noreply.github.com>
2024-09-10 11:59:03 +0200

    Update TS namespace highlighting.

--- lua/mannydark/highlighting/plugins/treesitter.lua
+++ lua/mannydark/highlighting/plugins/treesitter.lua
@@ -72,7 +72,7 @@ treesitter.setupHighlighting = function()
   highlight(0, '@tag.tsx',                            { fg = colors.turquoise, bg = 'NONE'                        })  -- Tags.
   highlight(0, '@tag.builtin.tsx',                    { fg = colors.blue,      bg = 'NONE'                        })  -- HTML tags.
   highlight(0, '@tag.delimiter.tsx',                  { fg = colors.white,     bg = 'NONE'                        })  -- < > of tags.
-  highlight(0, '@lsp.type.namespace.typescriptreact', { fg = colors.white,     bg = 'NONE'                        })  -- Namespace of types like 'React'.
+  highlight(0, '@lsp.type.namespace.typescriptreact', { fg = colors.turquoise, bg = 'NONE'                        })  -- Namespace of types like 'React'.
 
 
 
