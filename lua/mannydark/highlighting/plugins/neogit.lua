88a078e3884935bbfd7ede9dd8e05912ed238e97
Manuel-Hubertus Fay <MannyFay@users.noreply.github.com>
2024-10-04 12:16:26 +0200

    Update on NeoGit highlighting.

--- lua/mannydark/highlighting/plugins/neogit.lua
+++ lua/mannydark/highlighting/plugins/neogit.lua
@@ -16,9 +16,13 @@ neoGit.setupHighlighting = function()
   highlight(0, 'NeogitObjectId',        { fg = colors.blue,   bg = 'NONE' })  -- Commit hash.
   highlight(0, 'NeogitBranch',          { fg = colors.purple, bg = 'NONE' })  -- Branch name.
   highlight(0, 'NeogitStagedchanges',   { fg = colors.red,    bg = 'NONE' })  -- Staged changes.
-  highlight(0, 'NeogitUnstagedchanges', { fg = colors.red,    bg = 'NONE' })  -- Un-staged changes.
-  highlight(0, 'NeogitUntrackedfiles',  { fg = colors.red,    bg = 'NONE' })  -- Untracked files.
-  highlight(0, 'NeogitFold',            { fg = colors.blue,   bg = 'NONE' })
+  highlight(0, 'NeogitUnstagedchanges', { fg = colors.red,    bg = 'NONE', underline = true })  -- Un-staged changes fold heading.
+  highlight(0, 'NeogitChangeMunstaged', { fg = colors.red,    bg = 'NONE' })  -- 'Modified' flag.
+  highlight(0, 'NeogitChangeDunstaged', { fg = colors.red,    bg = 'NONE' })  -- 'Deleted' flag.
+  highlight(0, 'NeogitUntrackedfiles',  { fg = colors.red,    bg = 'NONE', underline = true })  -- Untracked files fold heading.
+  highlight(0, 'NeogitFold',            { fg = colors.blue,   bg = 'NONE' })  -- Heading of file folds.
+  highlight(0, 'NeogitStashes',         { fg = colors.red,    bg = 'NONE', underline = true })  -- Stashes fold heading.
+  highlight(0, 'NeogitRemote',          { fg = colors.green, bg = colors.purple })  -- Remote name.
 
   ----------------------- Not used by now:
   highlight(0, 'NeogitStash',           { fg = colors.green, bg = colors.greenLignt })
@@ -26,14 +30,10 @@ neoGit.setupHighlighting = function()
   highlight(0, 'NeogitRebasing',        { fg = colors.green, bg = colors.red })
   highlight(0, 'NeogitDiffDelete',      { fg = colors.green, bg = colors.redLight })
   highlight(0, 'NeogitRebaseDone',      { fg = colors.green, bg = colors.orange })
-  highlight(0, 'NeogitRemote',          { fg = colors.green, bg = colors.purple })
-  highlight(0, 'NeogitStashes',         { fg = colors.green, bg = colors.pink })
   highlight(0, 'NeogitUnmergedInto',    { fg = colors.green, bg = colors.gray })
   highlight(0, 'NeogitUnpulledFrom',    { fg = colors.green, bg = colors.white })
-  highlight(0, 'NeogitRecentcommits',   { fg = colors.green, bg = colors.turquoise })
   highlight(0, 'NeogitUnmergedchanges', { fg = colors.pink, bg = colors.greenLignt })
   highlight(0, 'NeogitUnpulledchanges', { fg = colors.pink, bg = colors.blue })
-  highlight(0, 'NeogitUnstagedchanges', { fg = colors.pink, bg = colors.turquoise })
 
 
 
