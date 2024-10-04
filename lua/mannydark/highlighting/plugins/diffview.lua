d2d786aeae31fd592ea911a63975f4fe2798aebb
Manuel-Hubertus Fay <MannyFay@users.noreply.github.com>
2024-10-04 13:12:59 +0200

    Update on NeoGit highlighting.

--- lua/mannydark/highlighting/plugins/diffview.lua
+++ lua/mannydark/highlighting/plugins/diffview.lua
@@ -13,13 +13,13 @@ local diffview  = {}
 diffview.setupHighlighting = function()
   ----------------------- Not used by now:
   highlight(0, 'DiffViewNormal',             { fg = colors.purple, bg = colors.blue })
-  highlight(0, 'DiffviewStatusAdded',        { fg = colors.purple, bg = colors.blue })
-  highlight(0, 'DiffviewStatusModified',     { fg = colors.purple, bg = colors.blue })
-  highlight(0, 'DiffviewStatusRenamed',      { fg = colors.purple, bg = colors.blue })
-  highlight(0, 'DiffviewStatusDeleted',      { fg = colors.purple, bg = colors.blue })
-  highlight(0, 'DiffviewFilePanelInsertion', { fg = colors.purple, bg = colors.blue })
-  highlight(0, 'DiffviewFilePanelDeletion',  { fg = colors.purple, bg = colors.blue })
-  highlight(0, 'DiffviewVertSplit',          { fg = colors.purple, bg = colors.blue })
+  highlight(0, 'DiffviewStatusAdded',        { fg = colors.purple, bg = colors.green })
+  highlight(0, 'DiffviewStatusModified',     { fg = colors.purple, bg = colors.greenLight })
+  highlight(0, 'DiffviewStatusRenamed',      { fg = colors.purple, bg = colors.red })
+  highlight(0, 'DiffviewStatusDeleted',      { fg = colors.purple, bg = colors.turquoise })
+  highlight(0, 'DiffviewFilePanelInsertion', { fg = colors.purple, bg = colors.orange })
+  highlight(0, 'DiffviewFilePanelDeletion',  { fg = colors.turquoise, bg = colors.pink })
+  highlight(0, 'DiffviewVertSplit',          { fg = colors.turquoise, bg = colors.blue })
 end
 
 return diffview
