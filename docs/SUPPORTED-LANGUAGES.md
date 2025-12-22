3da91068b0f99f6d7baef3e9e996daa947e1f4f9
MannyFay <mannythefay@gmail.com>
2025-12-22 12:57:31 +0100

    Add Markdown highlighting.

--- docs/SUPPORTED-LANGUAGES.md
+++ docs/SUPPORTED-LANGUAGES.md
@@ -9,6 +9,7 @@ If there is the need for one that is not listed here, please open an issue or a 
 - JavaScript
 - JSON
 - Lua
+- Markdown
 
 ### TODO/Refine
 - Ada
--- lua/mannydark/highlighting/languages/markdown.lua
+++ lua/mannydark/highlighting/languages/markdown.lua
@@ -11,7 +11,21 @@ local markdown  = {}
 -- Settings
 
 markdown.setupHighlighting = function()
-
+  highlight(0, "@markup.heading.1.markdown", { link = "Keyword" })
+  highlight(0, "@markup.heading.2.markdown", { link = "Keyword" })
+  highlight(0, "@markup.heading.3.markdown", { link = "Keyword" })
+  highlight(0, "@markup.heading.4.markdown", { link = "Keyword" })
+  highlight(0, "@markup.heading.5.markdown", { link = "Keyword" })
+  highlight(0, "@markup.heading.6.markdown", { link = "Keyword" })
+  highlight(0, "@conceal.markdown_inline", { link = "Keyword" })
+  highlight(0, "@lsp.type.class.markdown", { link = "MannydarkFgWhite" })
+  highlight(0, "@markup.link.markdown_inline", { link = "Keyword" })
+  highlight(0, "@markup.link.url.markdown_inline", { fg = colors.white, bg = "NONE", underline = true })
+  highlight(0, "@markup.link.url.markdown", { fg = colors.white, bg = "NONE", underline = true })
+  highlight(0, "@markup.raw.markdown_inline", { link = "MannydarkFgWhite" })
+  highlight(0, "@markup.raw.block.markdown", { link = "MannydarkFgWhite" })
+  highlight(0, "@punctuation.special.markdown", { link = "Keyword" })
+  highlight(0, "@markup.quote.markdown", { link = "MannydarkFgWhite" })
 end
 
 return markdown
