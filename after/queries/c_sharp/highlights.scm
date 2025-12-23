8b338a8161f446494c66d2efdb7ca86f624bf3d1
MannyFay <mannythefay@gmail.com>
2025-12-23 08:48:53 +0100

    Update Csharp highlighting on keywords.

--- after/queries/c_sharp/highlights.scm
+++ after/queries/c_sharp/highlights.scm
@@ -4,3 +4,24 @@
 ((_) @keyword.modifier
   (#any-of? @keyword.modifier "out" "ref" "in")
   (#set! priority 200))
+
+; LINQ join clause keywords
+(join_clause
+  "join" @keyword
+  (#set! priority 200))
+
+(join_clause
+  "in" @keyword
+  (#set! priority 200))
+
+(join_clause
+  "on" @keyword
+  (#set! priority 200))
+
+(join_clause
+  "equals" @keyword
+  (#set! priority 200))
+
+(join_into_clause
+  "into" @keyword
+  (#set! priority 200))
--- lua/mannydark/highlighting/languages/csharp.lua
+++ lua/mannydark/highlighting/languages/csharp.lua
@@ -3,7 +3,7 @@
 -- Highlighting for .cs, .csx files.
 -------------------------------------------------------------------------------
 
-local colors  = require("mannydark.palette")
+-- local colors  = require("mannydark.palette")
 local highlight = vim.api.nvim_set_hl
 local csharp  = {}
 
@@ -12,7 +12,9 @@ local csharp  = {}
 -- Settings
 
 csharp.setupHighlighting = function()
-
+  highlight(0, "@comment.documentation.c_sharp", { link = "MannydarkFgGreen" })  -- Doc blocks (/** ... */ or ///)
+  highlight(0, "@type.builtin.c_sharp",          { link = "Keyword" })           -- Built-in types (int, bool, string, etc.)
+  highlight(0, "@character.special.c_sharp",     { link = "@text" })             -- _
 end
 
 return csharp
